293850e5d7de040a332b6d2feab767aba455bb1f
Tafita Mathieu <hei.tafita.2@gmail.com>
2025-04-23 16:00:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       "http://127.0.0.1:5500",
       "http://localhost:5500",
-      "https://ma-driftype.netlify.app"",
+      "https://ma-driftype.netlify.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
